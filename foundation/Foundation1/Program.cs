a89d03e276d52554a2363f5d2e2197c88be61703
k13amanda <u232377037@byui.edu>
2024-11-18 14:49:52 -0700

    updates added a list

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,22 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+
+        List<Video> videos = new List<Video>();
 
         Video video1 = new Video("Cute Cats", "John Smith", 60);
+        video1.AddComment (new Comment("Alice", "I loved that video"));
+        video1.AddComment (new Comment("Adam", "That video was awesome"));
+        video1.AddComment (new Comment("Susan", "I love cats also"));
+        video1.AddComment (new Comment("Ben", "That was so cute"));
+        videos.Add(video1);
 
-        Comment comment1 = new Comment("Alice", "I loved that video");
+        Video video2 = new Video("Cute Dogs", "Alex Johnson", 70);
+        video2.AddComment (new Comment("Sam", "That dog video was awesome"));
+        video2.AddComment (new Comment("Sue", "I love dogs also"));
+        video2.AddComment (new Comment("Nephi", "Those were some cute dogs"));
+        videos.Add(video2);
 
-        video1.AddComment(comment1);
+        Video video3 = new Video("Cool Science", "Amy Adams", 110);
+        video3.AddComment (new Comment("Kaden", "I loved that video"));
+        video3.AddComment (new Comment("Landon", "That video was awesome"));
+        video3.AddComment (new Comment("Susan", "I love Science"));
+        video3.AddComment (new Comment("Adam", "That was so cool"));
+        videos.Add(video3);
 
-        Console.WriteLine ($"Title: {video1.GetTitle()}");
-        Console.WriteLine ($"Author: {video1.GetAuthor()}");
-        Console.WriteLine ($"Lenght: {video1.GetLength()} seconds");
-        Console.WriteLine($"Number of Comments: {video1.GetNumberOfComments()}");
+        Video video4 = new Video("How to make a Paper Airplane", "Frank Colter", 90);
+        video4.AddComment (new Comment("Jaxon", "I loved that first airplane"));
+        video4.AddComment (new Comment("Carson", "That video was so helpful"));
+        video4.AddComment (new Comment("Ben", "Those were some awesome airplanes"));
+        videos.Add(video4);
 
-        foreach (Comment comment in video1.GetComments())
-        {
-            Console.WriteLine($"{comment.GetName()}: {comment.GetText()}");
-        }
+
+        foreach (Video video in videos)
+            { 
+            Console.WriteLine($"Title: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAuthor()}");
+            Console.WriteLine($"Length: {video.GetLength()} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"{comment.GetName()}: {comment.GetText()}");
+            }
+
+        Console.WriteLine("");
+        }    
     }    
 }
