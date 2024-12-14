009deb9ff856d7bc022b77be84a2b59a8893f34f
k13amanda <u232377037@byui.edu>
2024-12-13 20:54:09 -0700

    updates add screenshot

--- foundation/Foundation3/Activity .cs
+++ foundation/Foundation3/Activity .cs
@@ -20,8 +20,6 @@ public abstract class Activity
     public string GetSummary()
     {
         return $"{_date:dd MMM yyyy} {_activity} ({_minutes} min) - Distance: {GetDistance():0.0} miles, Speed: {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
-        // 03 Nov 2022 Running (30 min)- Distance 3.0 miles, Speed 6.0 mph, Pace: 10.0 min per mile
-        // return $"{_date:dd MMM yyyy} {_activity} ({_minutes} min) - Distance {GetDistance} miles, Speed {GetSpeed} mph, Pace:{GetPace} min per mile";
     }
 
 
--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -7,14 +7,10 @@ class Program
         DateTime runDate = new DateTime(2022, 11, 03);
         Running run = new Running(runDate, "Running", 30, 3);
         Console.WriteLine(run.GetSummary());
-
-
    
         DateTime swimDate = new DateTime(2024, 10, 23);
         Swimming swim = new Swimming(swimDate, "Swimming", 45, 20);
         Console.WriteLine(swim.GetSummary());
-
-
 
         DateTime cycleDate = new DateTime(2024, 09, 15);
         Cycling cycling = new Cycling(cycleDate, "Cycling", 60, 15);
